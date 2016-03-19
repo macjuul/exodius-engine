3375ce4d869d68b4e3266b30510bfff3d1b5cd0f
Macjuul <minecraftmacjuul@gmail.com>
2016-03-19 01:13:12 +0100

    Completed Circle class for now

--- src/main/java/net/exodiusmc/engine/shape/Circle.java
+++ src/main/java/net/exodiusmc/engine/shape/Circle.java
@@ -5,4 +5,28 @@ import net.exodiusmc.engine.Location;
 public class Circle {
 	private Location center;
 	private double radius;
+	
+	public Circle() {
+	    this(new Location());
+	}
+	
+	public Circle(Location center) {
+	    this.center = center;
+	}
+
+    public Location getCenter() {
+        return center;
+    }
+
+    public void setCenter(Location center) {
+        this.center = center;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
