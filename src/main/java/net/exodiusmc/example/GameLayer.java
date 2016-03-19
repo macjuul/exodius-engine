f6c240d9a85d5cb831afc0ce584fc17970762cd6
Macjuul <minecraftmacjuul@gmail.com>
2016-03-19 01:01:48 +0100

    Added testing game

--- src/main/java/net/exodiusmc/example/GameLayer.java
+++ src/main/java/net/exodiusmc/example/GameLayer.java
@@ -1,16 +1,25 @@
 package net.exodiusmc.example;
 
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
 import net.exodiusmc.engine.layers.Layer;
+import net.exodiusmc.engine.util.FileUtils;
 
 public class GameLayer implements Layer {
+    private Image ground;
+    
+    public GameLayer() {
+        this.ground = FileUtils.LoadImage("ground.png");
+    }
+    
+    @Override
+    public void update(double delta, long frame) {
+        
+    }
 
 	@Override
-	public void update(GraphicsContext gfx) {
-		gfx.setFill(Color.GREEN);
-        gfx.fillRect(20, 20, 100, 100);
-        gfx.getCanvas().setLayoutX(100);
+	public void render(GraphicsContext gfx) {
+		gfx.drawImage(ground, 0, 0);
 	}
 
 	@Override
--- src/main/java/net/exodiusmc/example/Main.java
+++ src/main/java/net/exodiusmc/example/Main.java
@@ -3,15 +3,25 @@ package net.exodiusmc.example;
 import javafx.stage.Stage;
 import net.exodiusmc.engine.ExodiusEngine;
 import net.exodiusmc.engine.Runtime;
+import net.exodiusmc.engine.util.FileUtils;
 
 public class Main extends ExodiusEngine {
+    public static Stage window;
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 	@Override
 	public void init(Stage window, Runtime run) {
-		window.setHeight(300);
-		window.setWidth(400);
-		window.setTitle("Sample game");
+	    FileUtils.setResourceDirectory("net/exodiusmc/example");
+	    this.window = window;
+	    
+		window.setHeight(523);
+		window.setWidth(528);
+		window.setTitle("Monster hunt");
 		window.show();
+		
 		
 		GameLayer game = new GameLayer();
 		
@@ -19,6 +29,8 @@ public class Main extends ExodiusEngine {
 	}
 
 	@Override
-	public void update(double delta) {}
+	public void update(double delta) {
+	    
+	}
 	
 }
