7510d33b66daa4bb2313ac9d1d670baea5a994aa
Julian <minecraftmacjuul@gmail.com>
2016-03-31 17:22:37 +0200

    Changed Entity class

--- src/main/java/net/exodiusmc/example/entity/Entity.java
+++ src/main/java/net/exodiusmc/example/entity/Entity.java
@@ -11,7 +11,6 @@ public class Entity {
 	private double maxAcceleration = 5;
 	private double acceleration_X;
 	private double acceleration_Y;
-
 	public Direction facing;
 	
 	public Entity(Location pos) {
@@ -24,74 +23,58 @@ public class Entity {
 		return this.pos;
 	}
 
-	public void setPos(Location pos) {
+	public void setLocation(Location pos) {
 		this.prevPos = this.pos.clone();
 		this.pos = pos;
 	}
-	public void move(Direction d, double dt) {
-		move(d, dt, null);
+	
+	public void moveTo(Rectangle field, Location l) {
+		moveTo(field, l, false);
 	}
 	
-	public void move(Direction d, double dt, Location l) {
-		switch(d) {
-		case UP:
-			this.acceleration_Y = Math.max(acceleration_Y - movementSpeed, -this.maxAcceleration);
-			this.facing = Direction.UP;
-			break;
-		case RIGHT:
-			this.acceleration_X = Math.min(acceleration_X + movementSpeed, this.maxAcceleration);
-			this.facing = Direction.RIGHT;
-			break;
-		case DOWN:
-			this.acceleration_Y = Math.min(acceleration_Y + movementSpeed, this.maxAcceleration);
-			this.facing = Direction.DOWN;
-			break;
-		case LEFT:
-			this.acceleration_X = Math.max(acceleration_X - movementSpeed, -this.maxAcceleration);
-			this.facing = Direction.LEFT;
-			break;
-		case CUSTOM:
-			double toPlayerX = pos.getX() - l.getX();
-			double toPlayerY = pos.getY() - l.getY();
-			
-			double toPlayerLength = Math.sqrt(toPlayerX * toPlayerX + toPlayerY * toPlayerY);
-			toPlayerX = toPlayerX / toPlayerLength;
-			toPlayerY = toPlayerY / toPlayerLength;
-			
-			acceleration_X -= toPlayerX * dt * movementSpeed;
-			acceleration_Y -= toPlayerY * dt * movementSpeed;
-			
-			if(this.acceleration_X > this.maxAcceleration) {
-				this.acceleration_X = this.maxAcceleration;
-			} else if(this.acceleration_X < -this.maxAcceleration) {
-				this.acceleration_X = -this.maxAcceleration;
-			}
-			
-			if(this.acceleration_Y > this.maxAcceleration) {
-				this.acceleration_Y = this.maxAcceleration;
-			} else if(this.acceleration_Y < -this.maxAcceleration) {
-				this.acceleration_Y = -this.maxAcceleration;
-			}
+	public void moveTo(Rectangle field, Location l, boolean checkField) {
+		double toLocX = pos.getX() - l.getX();
+		double toLocY = pos.getY() - l.getY();
+		
+		double toPlayerLength = Math.sqrt(toLocX * toLocX + toLocY * toLocY);
+		toLocX = toLocX / toPlayerLength;
+		toLocY = toLocY / toPlayerLength;
+		
+		acceleration_X -= toLocX * movementSpeed;
+		acceleration_Y -= toLocY * movementSpeed;
+		
+		if(this.acceleration_X > this.maxAcceleration) {
+			this.acceleration_X = this.maxAcceleration;
+		} else if(this.acceleration_X < -this.maxAcceleration) {
+			this.acceleration_X = -this.maxAcceleration;
 		}
+		
+		if(this.acceleration_Y > this.maxAcceleration) {
+			this.acceleration_Y = this.maxAcceleration;
+		} else if(this.acceleration_Y < -this.maxAcceleration) {
+			this.acceleration_Y = -this.maxAcceleration;
+		}
+		
+		handleMovement(field, checkField);
 	}
 	
-	public void handleMovement(double dt, Rectangle field) {
-		handleMovement(dt, field, true);
-	}
-	
-	public void handleMovement(double dt, Rectangle field, boolean checkRect) {
-		saveLoc();
+	private void handleMovement(Rectangle field, boolean checkRect) {
+		saveLocation();
+		
 		this.pos.setX(this.pos.getX() + this.acceleration_X);
 		if(!field.contains(this.pos) && checkRect) {
-    		undoMovement();
+			restoreLocation();
     	}
+		
     	this.acceleration_X *= 0.8;
 		
-    	saveLoc();
+    	saveLocation();
+    	
 		this.pos.setY(this.pos.getY() + this.acceleration_Y);
 		if(!field.contains(this.pos) && checkRect) {
-    		undoMovement();
+    		restoreLocation();
     	}
+		
     	this.acceleration_Y *= 0.8;
 	}
 
@@ -103,11 +86,11 @@ public class Entity {
 		this.movementSpeed = movementSpeed;
 	}
 	
-	public void saveLoc() {
+	public void saveLocation() {
 		this.prevPos = this.pos.clone();
 	}
 	
-	public void undoMovement() {
+	public void restoreLocation() {
 		this.pos = this.prevPos.clone();
 	}
 }
