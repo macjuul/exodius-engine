752f0978a5719e1b8db38a4cf426103118fcce08
Julian <minecraftmacjuul@gmail.com>
2016-04-01 09:39:55 +0200

    Updated hero class

--- src/main/java/net/exodiusmc/example/entity/living/Hero.java
+++ src/main/java/net/exodiusmc/example/entity/living/Hero.java
@@ -8,8 +8,6 @@ import net.exodiusmc.example.entity.LivingEntity;
 import net.exodiusmc.example.layers.DeathLayer;
 
 public class Hero extends LivingEntity {
-	private int maxHealth = 10;
-	private int health = maxHealth;
 	private SpriteAnimation sprite;
 	
 	public Direction facing;
@@ -35,35 +33,7 @@ public class Hero extends LivingEntity {
 		return this.sprite;
 	}
 	
-	public void damage() {
-		this.damageTick = 1;
-		this.dmgTick = 1;
-		this.health--;
-		
-		if(this.health <= 0) {
-			death();
-			this.health = 0;
-		}
-	}
-	
 	public void death() {
 		Main.engine.getLayerManager().add(new DeathLayer());
-	}
-
-	public int getHealth() {
-		return health;
-	}
-	
-	public int getMaxHealth() {
-		return maxHealth;
-	}
-	
-	public void setMaxHealth(int maxHealth) {
-		this.maxHealth = maxHealth;
-	}
-
-	public void setHealth(int health) {
-		this.health = health;
-		if(this.health >= maxHealth) this.health = this.maxHealth;
 	}
 }
