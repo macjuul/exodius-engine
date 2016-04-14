8976337859b4ca4cad2ffb331a17633ffb23a96f
Julian <minecraftmacjuul@gmail.com>
2016-04-14 09:52:30 +0200

    Fixed missing files

---
+++ src/main/java/net/exodiusmc/example/SwordDirection.java
@@ -0,0 +1,22 @@
+package net.exodiusmc.example;
+
+public enum SwordDirection {
+	UP(0),
+	UP_RIGHT(45),
+	RIGHT(90),
+	RIGHT_DOWN(135),
+	DOWN(180),
+	DOWN_LEFT(225),
+	LEFT(270),
+	LEFT_UP(315);
+	
+	private int base;
+	
+	private SwordDirection(int base) {
+		this.base = base;
+	}
+	
+	public int getBaseRotation() {
+		return this.base;
+	}
+}
